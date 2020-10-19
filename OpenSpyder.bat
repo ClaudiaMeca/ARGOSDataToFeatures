baaef20a5689ccffaf710f077660aa002a62df4e
ClaudiaMeca <claudiaeia@gmail.com>
2020-10-19 16:48:03 -0400

    committing

---
+++ OpenSpyder.bat
@@ -0,0 +1,4 @@
+@set the_env=env859-py3
+call "C:\Users\clauv\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs\env859-py3\Scripts\activate.bat" "%the_env%"
+call "C:\Users\clauv\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs\env859-py3\Scripts\spyder.exe" -w %cd% 
+call "C:\Users\clauv\AppData\Local\Programs\ArcGIS\Pro\bin\Python\envs\env859-py3\Scripts\deactivate.bat"
